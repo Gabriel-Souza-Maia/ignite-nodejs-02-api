7c8f0a12be790c634a90420c6b78cfb6667bfad5
Gabriel Maia <gsmaia23@gmail.com>
2024-10-31 07:08:52 -0400

    adding fixed host for testing deploy

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import { app } from './app'
 import { env } from './env'
 
+const HOST = '0.0.0.0'
+
 app
   .listen({
     port: env.PORT,
+    host: HOST,
   })
   .then(() => {
     console.log(`Server is running on port: ${env.PORT}`)
